c649b14b1ca29a8b2d8e9f3601e4b27639bcc66a
Alexis Ravel <alexis.ravel@outlook.fr>
2023-02-06 16:26:23 +0100

    Ajout formulaire

--- src/Controller/BlogController.php
+++ src/Controller/BlogController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -10,11 +12,17 @@ use Symfony\Component\HttpFoundation\Request;
 use App\Repository\PostRepository;
 use App\Repository\CommentRepository;
 use App\Entity\Post;
+use App\Entity\Comment;
+use App\Form\CommentFormType;
 
 class BlogController extends AbstractController {
+    private EntityManagerInterface $entityManager;
+    public function __construct(EntityManagerInterface $entityManager) {
+        $this->entityManager = $entityManager;
+    }
+
     #[Route('/', name: 'app_blog', defaults: ['page'=>'1'], methods: ['GET'])]
     #[Route('/page/{page<[1-9]\d{0,8}>}', name: 'app_blog_page', methods: ['GET'])]
-
     public function index(Request $request, PostRepository $posts): Response {   
         $offset = max(0, $request->query->getInt('offset', 0));     
         $latestPosts = $posts->getCommentPaginator($offset);
@@ -31,12 +39,25 @@ class BlogController extends AbstractController {
     public function show(Request $request, Post $post, CommentRepository $commentRepository): Response {
         $offset = max(0, $request->query->getInt('offset', 0));
         $paginator = $commentRepository->getCommentPaginator($post, $offset);
+        $comment = new Comment();
+        $form = $this->createForm(CommentFormType::class, $comment);
+
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $comment->setPost($post);
+            $comment->setPublishedAt(new \DateTimeImmutable());
+            $this->entityManager->persist($comment);
+            $this->entityManager->flush();
+
+            return $this->redirectToRoute('publication', ['id'=>$post->getId()]);
+        }
         
         return $this->render('blog/show.html.twig', [
             'publication' => $post,
             'comments' => $paginator,
             'previous' => $offset - CommentRepository::PAGINATOR_PER_PAGE,
             'next' => min(count($paginator), $offset + CommentRepository::PAGINATOR_PER_PAGE),
+            'comment_form' => $form->createView(),
         ]);
     }
 }
