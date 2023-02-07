17975fcfe2b039a562f7f2aec6dd96a46cf11b63
Alexis Ravel <alexis.ravel@outlook.fr>
2023-02-07 12:23:36 +0100

    Ajout service pour gestion des roles dans le CRUD

--- src/Controller/Admin/UserCrudController.php
+++ src/Controller/Admin/UserCrudController.php
@@ -3,19 +3,21 @@
 namespace App\Controller\Admin;
 
 use App\Entity\User;
+use App\Services\RolesHelper;
 
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Config\{Action, Actions, Crud, KeyValueStore};
 use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
 use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+use EasyCorp\Bundle\EasyAdminBundle\Field\ChoiceField;
 
 use Symfony\Component\Form\Extension\Core\Type\{PasswordType, RepeatedType};
 use Symfony\Component\Form\{FormBuilderInterface, FormEvent, FormEvents};
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UserCrudController extends AbstractCrudController {
-    public function __construct(private UserPasswordHasherInterface $userPasswordHasher) {
+    public function __construct(private RolesHelper $rolesHelper, private UserPasswordHasherInterface $userPasswordHasher) {
 
     }
 
@@ -25,32 +27,41 @@ class UserCrudController extends AbstractCrudController {
 
     public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface {
         $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
-        return $this->addPasswordEventListener($formBuilder);
+        return $this->addFieldEventListener($formBuilder);
     }
 
     public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface {
         $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
-        return $this->addPasswordEventListener($formBuilder);
+        return $this->addFieldEventListener($formBuilder);
     }
 
-    private function addPasswordEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface {
-        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->hashPassword());
+    private function addFieldEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->manageField());
     }
 
-    private function hashPassword() {
+    private function manageField() {
         return function($event) {
             $form = $event->getForm();
-            if (!$form->isValid()) {
+            if(!$form->isValid()) {
                 return;
             }
             $password = $form->get('password')->getData();
-            if ($password === null) {
+            if($password === null) {
                 return;
             }
-            $hash = $this->userPasswordHasher->hashPassword($this->getUser(), $password);
-            $form->getData()->setPassword($hash);
+
+            $form->getData()->setPassword($this->hashPassword($password));
+            $form->getData()->setRoles($this->manageRoles($form));
         };
-    }       
+    }
+
+    private function hashPassword($password): string {
+        return $this->userPasswordHasher->hashPassword($this->getUser(), $password);
+    }
+    
+    private function manageRoles($form): array {
+        return array_values($form->get('roles')->getData());
+    }
 
     public function configureFields(string $pageName): iterable {
         yield TextField::new('username', "Nom d'utilisateur");
@@ -65,6 +76,8 @@ class UserCrudController extends AbstractCrudController {
             ])
             ->setRequired($pageName === Crud::PAGE_NEW)
             ->onlyOnForms();
+        
+        yield ChoiceField::new('roles')->setChoices($this->rolesHelper->getRoles())->allowMultipleChoices();
     }
 
     /*
