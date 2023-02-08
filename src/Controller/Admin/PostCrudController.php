aab27839a0a72034f2553e1f44f29c471dde97e0
Alexis Ravel <alexis.ravel@outlook.fr>
2023-02-08 13:53:10 +0100

    Ajout setAuteur lors de la création d'un post en fonction de la connexion

--- src/Controller/Admin/PostCrudController.php
+++ src/Controller/Admin/PostCrudController.php
@@ -4,15 +4,45 @@ namespace App\Controller\Admin;
 
 use App\Entity\Post;
 use DateTime;
-use EasyCorp\Bundle\EasyAdminBundle\Config\Crud;
+
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\AssociationField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextareaField;
+use EasyCorp\Bundle\EasyAdminBundle\Config\{Crud, KeyValueStore};
+use EasyCorp\Bundle\EasyAdminBundle\Context\AdminContext;
+use EasyCorp\Bundle\EasyAdminBundle\Dto\EntityDto;
+
+use Symfony\Component\Form\{FormBuilderInterface, FormEvents};
 
 class PostCrudController extends AbstractCrudController {
     public static function getEntityFqcn(): string {
         return Post::class;
+    }
+
+    public function createNewFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface {
+        $formBuilder = parent::createNewFormBuilder($entityDto, $formOptions, $context);
+        return $this->addFieldEventListener($formBuilder);
+    }
+
+    public function createEditFormBuilder(EntityDto $entityDto, KeyValueStore $formOptions, AdminContext $context): FormBuilderInterface {
+        $formBuilder = parent::createEditFormBuilder($entityDto, $formOptions, $context);
+        return $this->addFieldEventListener($formBuilder);
+    }
+
+    private function addFieldEventListener(FormBuilderInterface $formBuilder): FormBuilderInterface {
+        return $formBuilder->addEventListener(FormEvents::POST_SUBMIT, $this->manageField());
+    }
+
+    private function manageField() {
+        return function($event) {
+            $form = $event->getForm();
+            if(!$form->isValid()) {
+                return;
+            }
+
+            $form->getData()->setAuteur($this->getUser());
+        };
     }
 
     public function configureCrud(Crud $crud): Crud {
